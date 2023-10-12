5beb93eb36378e15f9bf87cf2b614caf9c275377
mrcnbunyamin <mrcnbunyamin@gmail.com>
2023-10-12 11:25:40 +0300

    useRef #1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState, useEffect, useRef } from 'react'
 function App() {
+
+  const [input, setInput] = useState("");
+  const count = useRef(1);
+
+
+  useEffect(() => {
+    //setCount(prev => prev+1)
+    count.current += 1;
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type='text' value={input} onChange={e => setInput(e.target.value)} />
+      <button >Click ME!</button>
+      <div>You entered {input}</div>
+
+      <div>Count is: {count.current}</div>
     </div>
   );
 }
