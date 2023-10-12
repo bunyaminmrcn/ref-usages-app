9aaa6a12614d1246befcd7da22f25e32bc1ec5f5
mrcnbunyamin <mrcnbunyamin@gmail.com>
2023-10-12 11:34:46 +0300

    useRef #2

--- src/App.js
+++ src/App.js
@@ -5,19 +5,26 @@ function App() {
 
   const [input, setInput] = useState("");
   const count = useRef(1);
-
+  const inputRef = useRef();
 
   useEffect(() => {
     //setCount(prev => prev+1)
     count.current += 1;
   })
 
+
+  const clickHandler = () => {
+    console.log(inputRef.current)
+    const value = inputRef.current.value;//html element value
+    if(!value) inputRef.current.focus();
+    else alert(value)
+  }
   return (
     <div className="App">
-      <input type='text' value={input} onChange={e => setInput(e.target.value)} />
-      <button >Click ME!</button>
-      <div>You entered {input}</div>
+      <input ref={inputRef} type='text' value={input} onChange={e => setInput(e.target.value)} />
+      <button onClick={clickHandler}>Click ME!</button>
 
+      <div>You entered {input}</div>
       <div>Count is: {count.current}</div>
     </div>
   );
